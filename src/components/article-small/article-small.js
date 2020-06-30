6a139d1735bdb0d0c61f827a853860009d703a03
Nick Sotula <so2niko@gmail.com>
2020-06-30 19:30:32 +0300

    fix: class to classname in article-small

--- src/components/article-small/article-small.js
+++ src/components/article-small/article-small.js
@@ -4,16 +4,16 @@ import './article-small.css';
 
 function ArticleSmall(props){
     return (
-        <div class="card bg-dark text-white article-small">
+        <div className="card bg-dark text-white article-small">
             <a 
                         className="article-title"
                         href="https://www.cyberscoop.com/alexander-vinnik-russia-extradition-laundering/">
                 <img 
                     src="https://s3-us-west-2.amazonaws.com/cyberscoop-media/wp-content/uploads/2018/08/08153007/bitcoin-2007912_1920.jpg" 
-                    class="card-img" 
+                    className="card-img" 
                     alt="New Zealand freezes $90 million connected to accused bitcoin launderer Alexander Vinnik" />
-                <div class="card-img-overlay">
-                    <p class="card-text">
+                <div className="card-img-overlay">
+                    <p className="card-text">
                         <span className="article-title">
                             New Zealand freezes $90 million connected to accused bitcoin launderer Alexander Vinnik
                         </span>
