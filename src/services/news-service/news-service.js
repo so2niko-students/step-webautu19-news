74a9dd78cf7e511ebc37ba18c7708a1aa6f37570
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-07-14 21:36:56 +0300

    Update news-service.js

--- src/services/news-service/news-service.js
+++ src/services/news-service/news-service.js
@@ -2,11 +2,12 @@ export default class NewsService{
     apiUrl = 'https://newsapi.org/v2/top-headlines?';
     apiKey = '58d7638951004aa495513f5703d744f7';
 
-    getNews(){
+    async getNews(){
         const url = `${this.apiUrl}apiKey=${this.apiKey}&country=ua`;
-        return fetch(url)
-                .then(req => req.json())
-                .then(data => this.prepareArticles(data));
+        const res = fetch(url);
+        const body = res.json();
+        
+        this.prepareArticles(body);
     }
 
     prepareArticles(req){
