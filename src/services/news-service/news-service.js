0c25b9ec65c3b028b1f6999776b431c7d73f2b1b
payoneercashout <56314279+payoneercashout@users.noreply.github.com>
2020-07-14 21:35:21 +0300

    add async

--- src/services/news-service/news-service.js
+++ src/services/news-service/news-service.js
@@ -2,22 +2,25 @@ export default class NewsService{
     apiUrl = 'https://newsapi.org/v2/top-headlines?';
     apiKey = '58d7638951004aa495513f5703d744f7';
 
-    getNews(){
-        const url = `${this.apiUrl}apiKey=${this.apiKey}&country=ua`;
-        return fetch(url)
-                .then(req => req.json())
-                .then(data => this.prepareArticles(data));
+    async getNews(){
+       const url = `${this.apiUrl}apiKey=${this.apiKey}&country=ua`;
+      
+       const res = await fetch(url);
+       const body = await res.json();
+       
+       return this.prepareArticles(body);
     }
     
     prepareArticles(req){
         return req.articles;
     }
 
-    getNewsSmall(){
-        const url = `${this.apiUrl}country=ua&category=sports&apiKey=${this.apiKey}`;
-        return fetch(url)
-                .then(req => req.json())
-                .then(data => this.prepareArticles(data));
+    async getNewsSmall(){
+       const url = `${this.apiUrl}country=ua&category=sports&apiKey=${this.apiKey}`;
+       const res = await fetch(url);
+       const body = await res.json();
+       
+       return this.prepareArticles(body);
     }
    
 }
